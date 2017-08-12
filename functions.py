ba9c525fb5dc4ade93c550d69a97f9debf2a7635
andrzejmp <itstuff@ematmac.pl>
2017-08-12 09:31:18 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,4 +1,20 @@
 #functions.py
+
+def get_countries(fin):
+    '''
+    Reads a text file with fields separeted
+    by comma and returns a list of rows.
+    Each row a is a list and represents
+    one line from a text file.
+    '''
+    result = []
+    lines = fin.readlines()
+    for line in lines:
+        row = []
+        row = line.split(",")
+        result.append(row)
+    return result[1:]
+
 def text_to_list(fin):  
     '''
     Reads a text file with fields separeted 
@@ -29,5 +45,4 @@ def insert_to_countries1(nam, pop, are, p_90, p_00, p_10, fert, life, cont_id):
 
 s = '''INSERT INTO  countries (country_name, population, area, gdp_1990, gdp_2000, gdp_2010, fertility, life_exp, continent_id) 
        VALUES (:nam, :pop, :are, :p_90, :p_00, :p_10, :fert, :life, :cont_id);
-    '''     	
-    
+    ''' 
